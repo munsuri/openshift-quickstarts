d4b4d83a5a7bcc6ee9279aa93961468270c62c5e
Iván Munsuri Ibañez <munsuri@gmail.com>
2017-06-13 15:54:22 -0500

    test

--- undertow-servlet/src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ undertow-servlet/src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -24,7 +24,7 @@ public class HelloWorldVerticle extends AbstractVerticle{
   public void start(){
     logger.info( "start" );
     //System.setProperty( "java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %2$s [%4$s] %5$s%6$s%n" );
-    initMongo();
+    //initMongo();
     initServer();
   }
 
